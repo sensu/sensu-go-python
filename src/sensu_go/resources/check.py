687a733198d7f6cef2825f9efac6dbcef8fbc915
Tadej Borovšak <tadej.borovsak@xlab.si>
2020-12-10 19:16:50 +0100

    Add check resource

---
+++ src/sensu_go/resources/check.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2020 XLAB Steampunk
+
+from sensu_go.resources.namespaced import NamespacedResource
+from sensu_go.resources.v2 import V2Mixin
+
+
+class Check(V2Mixin, NamespacedResource):
+    PATH_TEMPLATE = "/api/core/v2/namespaces/{namespace}/checks"
