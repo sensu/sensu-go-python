5b9d90ec4aa25911e1e5b49829a2b27a26df2594
Tadej Borovšak <tadej.borovsak@xlab.si>
2021-03-12 00:45:34 +0100

    Add handler resource

--- src/sensu_go/clients/root.py
+++ src/sensu_go/clients/root.py
@@ -12,6 +12,7 @@ from sensu_go.resources.asset import Asset
 from sensu_go.resources.check import Check
 from sensu_go.resources.entity import Entity
 from sensu_go.resources.filter import Filter
+from sensu_go.resources.handler import Handler
 from sensu_go.resources.hook import Hook
 from sensu_go.resources.silence import Silence
 
@@ -51,6 +52,7 @@ class Client:
         self.checks = NamespacedClient(self._client, Check, ns)
         self.entities = NamespacedClient(self._client, Entity, ns)
         self.filters = NamespacedClient(self._client, Filter, ns)
+        self.handlers = NamespacedClient(self._client, Handler, ns)
         self.hooks = NamespacedClient(self._client, Hook, ns)
         self.silences = NamespacedClient(self._client, Silence, ns)
 
