cbbc3074c1a7d12035ab9b7931c3b13f5ed866cc
Tadej Borovšak <tadej.borovsak@xlab.si>
2021-03-12 00:54:48 +0100

    Add secret resource

---
+++ src/sensu_go/resources/secret.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2021 XLAB Steampunk
+
+from sensu_go.resources.namespaced import NamespacedResource
+from sensu_go.resources.v1 import V1Mixin
+
+
+class Secret(V1Mixin, NamespacedResource):
+    PATH_TEMPLATE = "/api/enterprise/secrets/v1/namespaces/{namespace}/secrets"
+    TYPE = "Secret"
+    API_VERSION = "secrets/v1"
