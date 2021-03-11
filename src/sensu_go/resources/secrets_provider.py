8a016ac2aa9e077351f9021dd9718bc535c33db6
Tadej Borovšak <tadej.borovsak@xlab.si>
2021-03-12 00:53:22 +0100

    Add secrets provider resource

---
+++ src/sensu_go/resources/secrets_provider.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2020 XLAB Steampunk
+
+from sensu_go.resources.cluster import ClusterResource
+from sensu_go.resources.v1 import V1Mixin
+
+
+class SecretsProvider(V1Mixin, ClusterResource):
+    PATH_TEMPLATE = "/api/enterprise/secrets/v1/providers"
+    TYPE = ""  # We set the type to an empty string because user needs to specify it
+    API_VERSION = "secrets/v1"
