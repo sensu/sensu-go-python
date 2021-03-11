4b9fc87e4983db5f8673a5234b81902c86caa3d4
Tadej Borovšak <tadej.borovsak@xlab.si>
2021-03-12 00:44:19 +0100

    Add filter resource

---
+++ src/sensu_go/resources/filter.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2021 XLAB Steampunk
+
+from sensu_go.resources.namespaced import NamespacedResource
+from sensu_go.resources.v2 import V2Mixin
+
+
+class Filter(V2Mixin, NamespacedResource):
+    PATH_TEMPLATE = "/api/core/v2/namespaces/{namespace}/filters"
+    TYPE = "EventFilter"
+    API_VERSION = "core/v2"
