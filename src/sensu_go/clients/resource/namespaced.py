e5eb8e55b733bd21af8752997b7d9a69c91b53c1
Tadej Borovšak <tadej.borovsak@xlab.si>
2021-03-12 16:20:08 +0100

    Use default namespace when creating resources
    Up until now, users  were required to provide a namespace even if the
    resource they are creating should live in a default namespace (as
    specified when the client was created). This commit allows users to
    omit the namespace metadata in those cases.

--- src/sensu_go/clients/resource/namespaced.py
+++ src/sensu_go/clients/resource/namespaced.py
@@ -1,10 +1,14 @@
 # Copyright (c) 2020 XLAB Steampunk
 
-from typing import List, Optional, Type
+from typing import List, Optional, Type, TypeVar
 
 from sensu_go.clients.http.base import HTTPClient
 from sensu_go.clients.resource.base import ResourceClient
+from sensu_go.resources.base import Resource
 from sensu_go.resources.namespaced import NamespacedResource as NsResource
+from sensu_go.typing import JSONItem
+
+T = TypeVar("T", bound=Resource)
 
 
 class NamespacedClient(ResourceClient):
@@ -16,6 +20,16 @@ class NamespacedClient(ResourceClient):
     ) -> None:
         super().__init__(client, resource_class)
         self._default_ns = default_namespace
+
+    def create(
+        self,
+        spec: JSONItem,
+        metadata: JSONItem,
+        type: Optional[str] = None,
+    ) -> T:
+        if "namespace" not in metadata:
+            metadata = dict(metadata, namespace=self._default_ns)
+        return super().create(spec, metadata, type)
 
     def _get_path(self, ns: Optional[str], name: Optional[str] = None) -> str:
         return self.resource_class.get_path(namespace=ns or self._default_ns, name=name)
