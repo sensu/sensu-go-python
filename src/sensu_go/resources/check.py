c210429e39fbf925a345fd67206d26f617e28928
Tadej Borovšak <tadej.borovsak@xlab.si>
2021-03-15 23:48:18 +0100

    Make it possible to list events associate with a check

--- src/sensu_go/resources/check.py
+++ src/sensu_go/resources/check.py
@@ -1,5 +1,8 @@
 # Copyright (c) 2020 XLAB Steampunk
 
+from sensu_go.clients.resource.base import ResourceIter
+from sensu_go.clients.resource.operator import Equal
+from sensu_go.resources.event import Event
 from sensu_go.resources.namespaced import NamespacedResource
 from sensu_go.resources.v2 import V2Mixin
 
@@ -9,3 +12,12 @@ class Check(V2Mixin, NamespacedResource):
     TYPE = "CheckConfig"
     API_VERSION = "core/v2"
     FIELD_PREFIX = "check"
+
+    @property
+    def events(self) -> ResourceIter[Event]:
+        return ResourceIter[Event](
+            Event,
+            self._client,
+            Event.get_path(namespace=self.namespace),
+            field_selector=Equal("check.name", self.name),
+        )
